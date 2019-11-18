63dc0f70e4f36fb6ee7aa2bb2623cc7456505787
Hajime MATSUMOTO <m@hajime.work>
2019-11-18 20:01:47 +0900

    update

--- src/Logger.php
+++ src/Logger.php
@@ -11,6 +11,16 @@ class Logger implements Psr\Log\LoggerInterface
     use Psr\Log\LoggerTrait;
 
     /**
+     * @var Writer[]
+     */
+    private $writers = [];
+
+    /**
+     * @var array
+     */
+    private $context = [];
+
+    /**
      * Logs with an arbitrary level.
      *
      * @param mixed  $level
@@ -23,58 +33,33 @@ class Logger implements Psr\Log\LoggerInterface
      */
     public function log($level, $message, array $context = array())
     {
-        $log = (new LogFactory)($level, $message, $context);
+        $log = (new LogFactory)(
+            $level,
+            $message,
+            $context += $this->context
+        );
 
         foreach ($this->writers as $writer) {
             $writer->write($log);
         }
     }
 
-    private $writers = [];
-
+    /**
+     * Writerを追加
+     */
     public function addWriter(Writer $writer)
     {
         $this->writers[] = $writer;
     }
 
-    // /**
-    //  * PHPのエラーをログに変換する
-    //  */
-    // public function phpErrorReport($eno, $emsg, $efile, $eline)
-    // {
-    //     $this->log(call_user_func_array([$this, "createLog"], [
-    //         LogLevel::convertPHPErrorNo($eno),
-    //         [
-    //             "message" => $emsg,
-    //             "error_reporting" => error_reporting(),
-    //             "php_error_no" => $eno,
-    //             "php_error_file" => $efile,
-    //             "php_error_line" => $eline
-    //         ]
-    //     ]));
-    // }
-    //
-    // /**
-    //  * PHPのエクセプションをログに変換する
-    //  */
-    // public function phpException(\Throwable $exception)
-    // {
-    //     $level = Log::LEVEL_ERROR;
-    //     if ($exception instanceof LoggableExceptionInterface) {
-    //         $level = $exception->getPriority();
-    //     }
-    //
-    //     $log = call_user_func_array([$this, "createLog"], [
-    //         $level,
-    //         [
-    //             "message" => (string) $exception->getMessage(),
-    //             "file" => (string) $exception->getFile(),
-    //             "line" => (string) $exception->getLine(),
-    //             "exception" => (string) get_class($exception),
-    //             "trace" =>  explode("\n", $exception->getTraceAsString())
-    //         ]
-    //     ]);
-    //     $log->category = get_class($exception);
-    //     $this->log($log);
-    // }
+    /**
+     * コンテクストを作成する
+     */
+    public function context(array $context)
+    {
+        $logger = new static();
+        $logger->writers = $this->writers;
+        $logger->context = $this->context + $context;
+        return $logger;
+    }
 }
